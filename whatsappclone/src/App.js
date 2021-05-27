5872fa680d1c14512f08a0688f34a66a0e9a5163
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-05-27 16:01:10 -0400

    criação do Sinebar header
    Criação e elaboração do avatar e icons do header

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function App() {
-  return (
-    <>
-      <h1>Nelcael</h1>
-    </>
-  );
+  return(
+    <div className='app-window'>
+      <div className="sidebar">
+        <header>
+           <img className='header--avatar' src='https://www.w3schools.com/howto/img_avatar2.png' alt=""></img>
+           <div className="header-buttons">
+              <div className="header-btn">
+                <DonutLargeIcon style={{color:'#919191'}}></DonutLargeIcon>
+              </div>
+              <div className="header-btn">
+                <ChatIcon style={{color:'#919191'}}></ChatIcon>
+              </div>
+              <div className="header-btn">
+                <MoreVertIcon style={{color:'#919191'}}></MoreVertIcon>
+              </div>
+          </div>
+        </header>
+        <div className='search'>
+            pesquisa
+        </div>
+        <div className='chatlist'>
+            lista de chat
+        </div>
+      </div>
+      <div className="contentarea">
+        segunda divisão
+      </div>
+    </div>      
+  )
 }
 
 export default App;
