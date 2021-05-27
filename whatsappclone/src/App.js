ba8231f916853bdd733d0cc61e5983474f887204
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-05-27 16:56:41 -0400

    Criação do search e começo da list de conversas

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import ChatListItem from './components/ChatListItem/ChatListItem';
 
 function App() {
+
+  const [chatlist, setChatList] = useState([{},{},{},{}])
+
   return(
     <div className='app-window'>
       <div className="sidebar">
-        <header>
-           <img className='header--avatar' src='https://www.w3schools.com/howto/img_avatar2.png' alt=""></img>
-           <div className="header-buttons">
-              <div className="header-btn">
-                <DonutLargeIcon style={{color:'#919191'}}></DonutLargeIcon>
-              </div>
-              <div className="header-btn">
-                <ChatIcon style={{color:'#919191'}}></ChatIcon>
-              </div>
-              <div className="header-btn">
-                <MoreVertIcon style={{color:'#919191'}}></MoreVertIcon>
-              </div>
+          <header>
+            <img className='header--avatar' src='https://www.w3schools.com/howto/img_avatar2.png' alt=""></img>
+            <div className="header-buttons">
+                <div className="header-btn">
+                  <DonutLargeIcon style={{color:'#919191'}}></DonutLargeIcon>
+                </div>
+                <div className="header-btn">
+                  <ChatIcon style={{color:'#919191'}}></ChatIcon>
+                </div>
+                <div className="header-btn">
+                  <MoreVertIcon style={{color:'#919191'}}></MoreVertIcon>
+                </div>
+            </div>
+          </header>
+
+          <div className='search'>
+            <div className="search--input">
+              <SearchIcon font-size='small' style={{color:'#919191'}}></SearchIcon>
+              <input type='search' placeholder='Procurar ou começar uma nova conversa'></input>
+            </div>    
           </div>
-        </header>
-        <div className='search'>
-            pesquisa
-        </div>
-        <div className='chatlist'>
-            lista de chat
-        </div>
+
+          <div className='chatlist'>
+            {chatlist.map((item,key)=>(
+              <ChatListItem key={key}></ChatListItem>
+            ))}
+          </div>
+
       </div>
       <div className="contentarea">
-        segunda divisão
+            segunda divisão
+
+
+            
       </div>
     </div>      
   )
---
+++ whatsappclone/src/components/ChatListItem/ChatListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatListItem.css'
+function ChatListItem() {
+    return(
+        <div>
+            testando componente de conversa
+        </div>
+    )
+}
+export default ChatListItem;
+
+
